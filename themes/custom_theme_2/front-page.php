96204f72f9be19f1d1a34cf1df9219892db84b4a
Morzina Pervin <76468425+morzinapervin@users.noreply.github.com>
2024-10-11 00:59:09 +0600

    third commit

--- themes/custom_theme_2/front-page.php
+++ themes/custom_theme_2/front-page.php
@@ -4,7 +4,7 @@ get_header();
 <main id="primary" class="site-main">
 	<section class="hm-hero hero-section">
 
-this is theme 2
+this is theme 2. Writing this lines for testing more
 	</section>
 </main>
 <?php
